e885b79970b2c09a8a990452155524a193892d02
chahat <io6.chahat1@gmail.com>
2024-05-04 20:12:55 +0530

    all commit

---
+++ Assignments/assignment-js2/script1.js
@@ -0,0 +1,13 @@
+// function personalInfo() {
+//     let name = "Chahat Rajput";
+//     let hobbies = ["Reading", "Learning new things", "Painting", "Dancing"];
+  
+//     console.log("Name: " + name);
+//     console.log("Hobbies:");
+//     hobbies.forEach(hobby => {
+//       console.log("- " + hobby);
+//     });
+//   }
+  
+
+//   personalInfo();
